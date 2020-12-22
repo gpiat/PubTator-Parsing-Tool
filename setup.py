56934b1a35d595f22298a4ad7c73a8a33e33bfa4
gpiat <guilhem.piat@cea.fr>
2020-12-22 12:38:18 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
-import nltk
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PubTator-gpiat",  # Replace with your own username
-    version="0.0.1",
+    name="pubtatortool",
+    version="0.1",
     author="Guilhem Piat",
     author_email="guilhem.piat@protonmail.com",
     description="A package for loading and manipulating PubTator files "
                 "as Python objects.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gpiat/PubTator-Parser",
+    url="https://github.com/gpiat/PubTator-Parsing-Tool",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Unlicense",
+        "License :: Public Domain",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
-
-nltk.download('punkt')
